3844ca9c0cbe348bc5d041e5888254b0e231a575
Muhammed Bilal <bilalmuhammed363@gmail.com>
2023-03-27 11:50:49 +0530

    fetched api

---
+++ lib/domain/core/api_end_points.dart
@@ -0,0 +1,8 @@
+import 'package:netflix_clone/core/strings.dart';
+import 'package:netflix_clone/infrastructure/api_key.dart';
+
+class ApiEndpoints {
+  static const downloads =
+      "$kbaseUrl/all/day?api_key=$apikey";
+}
+  //  "/trending/all/day?api_key=c2e2608024beabac09aa26c5fb840229"
